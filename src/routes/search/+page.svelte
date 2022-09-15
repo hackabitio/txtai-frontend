ebe45f91bdbf45b3e1c6fc2eee52d0317d9c36e3
foadyousefi <foadyousefi@gmail.com>
2022-09-15 14:39:15 +0200

    Move loading css to app.scss

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -35,7 +35,7 @@
 	</form>
 	<div class="searching">
 		{#if searching && !searchResults.length}
-			<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+			<div class="loading"><div></div><div></div><div></div><div></div></div>
 		{/if}
 	</div>
 	{#if searchResults.length}
@@ -92,67 +92,5 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-	}
-
-	.lds-ellipsis {
-		display: inline-block;
-		position: relative;
-		width: 80px;
-		height: 80px;
-
-		div {
-			position: absolute;
-			top: 33px;
-			width: 10px;
-			height: 10px;
-			border-radius: 50%;
-			background: tomato;
-			animation-timing-function: cubic-bezier(0, 1, 1, 0);
-
-			&:nth-child(1) {
-				left: 8px;
-				animation: lds-ellipsis1 0.6s infinite;
-			}
-
-			&:nth-child(2) {
-				left: 8px;
-				animation: lds-ellipsis2 0.6s infinite;
-			}
-
-			&:nth-child(3) {
-				left: 32px;
-				animation: lds-ellipsis2 0.6s infinite;
-			}
-
-			&:nth-child(4) {
-				left: 56px;
-				animation: lds-ellipsis3 0.6s infinite;
-			}
-		}
-	}
-
-	@keyframes lds-ellipsis1 {
-		0% {
-			transform: scale(0);
-		}
-		100% {
-			transform: scale(1);
-		}
-	}
-	@keyframes lds-ellipsis3 {
-		0% {
-			transform: scale(1);
-		}
-		100% {
-			transform: scale(0);
-		}
-	}
-	@keyframes lds-ellipsis2 {
-		0% {
-			transform: translate(0, 0);
-		}
-		100% {
-			transform: translate(24px, 0);
-		}
 	}
 </style>
